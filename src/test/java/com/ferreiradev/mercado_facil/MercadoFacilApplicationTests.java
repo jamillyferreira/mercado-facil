4ae6a506cf9fe50c1416bb2437c31fe352e5be40
jamillyferreira <jamillyferreira039@gmail.com>
2026-07-23 16:37:54 -0400

    first commit: Inicializa projeto mercado facil

---
+++ src/test/java/com/ferreiradev/mercado_facil/MercadoFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreiradev.mercado_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
